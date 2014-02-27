94228c565598adc0b4b3df6fb16ffa49f7a6d9ac
Parker, Eric P <eric_parker@harvard.edu>
2014-02-27 15:50:58 -0500

    changed port in gunicorm files

--- gunicorn_test.py
+++ gunicorn_test.py
@@ -2,14 +2,14 @@ import multiprocessing
 
 workers = multiprocessing.cpu_count() * 2 + 1
 
-bind = "127.0.0.1:8001"
+bind = "127.0.0.1:8004"
 
 '''
 daemon = True
 
 pidfile = 'gunicorn.pid'
 '''
-pythonpath = '/home/ubuntu/icommons_tools'
+pythonpath = '/home/ubuntu/icommons_ext_tools'
 
 
 accesslog = 'access.log'
