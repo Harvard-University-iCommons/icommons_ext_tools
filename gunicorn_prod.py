00888e645fee8e05c8890d1f1b430979677b5364
Parker, Eric P <eric_parker@harvard.edu>
2014-02-27 15:47:04 -0500

    changed port in gunicorm files

--- gunicorn_prod.py
+++ gunicorn_prod.py
@@ -5,13 +5,13 @@ workers = multiprocessing.cpu_count() * 2 + 1
 '''
 Point to local apache 
 '''
-bind = "127.0.0.1:8183"
+bind = "127.0.0.1:8184"
 #bind = "10.35.1.56:8183"
 
 #daemon = True
 
-pidfile = '/logs/icommons_tools/gunicorn.pid'
+pidfile = '/logs/icommons_ext_tools/gunicorn.pid'
 
-accesslog = '/logs/icommons_tools/gunicorn_access.log'
+accesslog = '/logs/icommons_ext_tools/gunicorn_access.log'
 
-errorlog = '/logs/icommons_tools/gunicorn_error.log'
+errorlog = '/logs/icommons_ext_tools/gunicorn_error.log'
