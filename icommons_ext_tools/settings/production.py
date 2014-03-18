b056d16b4c9ddd562b46c7834b701932dd512d6f
Parker, Eric P <eric_parker@harvard.edu>
2014-03-18 14:23:23 -0400

    try to get running in prod

--- icommons_ext_tools/settings/production.py
+++ icommons_ext_tools/settings/production.py
@@ -12,19 +12,19 @@ ALLOWED_HOSTS = ['*']
 
 ICOMMONS_COMMON = {
     'ICOMMONS_API_HOST': 'https://isites.harvard.edu/services/',
-    'ICOMMONS_API_USER': SECURE_SETTINGS['icommons_api_user'],
-    'ICOMMONS_API_PASS': SECURE_SETTINGS['icommons_api_pass'],
+    'ICOMMONS_API_USER': SECURE_SETTINGS.get('icommons_api_user'),
+    'ICOMMONS_API_PASS': SECURE_SETTINGS.get('icommons_api_pass'),
 }
 
 QUALTRICS_LINK = {
-    'AGREEMENT_ID' : SECURE_SETTINGS['qualtrics_agreement_id'],
-    'QUALTRICS_APP_KEY' : SECURE_SETTINGS['qualtrics_app_key'],
-    'QUALTRICS_API_URL' : SECURE_SETTINGS['qualtrics_api_url'], 
-    'QUALTRICS_API_USER' : SECURE_SETTINGS['qualtrics_api_user'], 
-    'QUALTRICS_API_TOKEN' : SECURE_SETTINGS['qualtrics_api_token'], 
-    'QUALTRICS_AUTH_GROUP' : SECURE_SETTINGS['qualtrics_auth_group'],
-    'USER_DECLINED_TERMS_URL' : 'ql:internal', # only in QA
-    'USER_ACCEPTED_TERMS_URL' : 'ql:internal', # only in QA
+    'AGREEMENT_ID' : SECURE_SETTINGS.get('qualtrics_agreement_id'),
+    'QUALTRICS_APP_KEY' : SECURE_SETTINGS.get('qualtrics_app_key'),
+    'QUALTRICS_API_URL' : SECURE_SETTINGS.get('qualtrics_api_url'), 
+    'QUALTRICS_API_USER' : SECURE_SETTINGS.get('qualtrics_api_user'), 
+    'QUALTRICS_API_TOKEN' : SECURE_SETTINGS.get('qualtrics_api_token'), 
+    'QUALTRICS_AUTH_GROUP' : SECURE_SETTINGS.get('qualtrics_auth_group'),
+    'USER_DECLINED_TERMS_URL' : 'http://surveytools.harvard.edu', # only in QA
+    'USER_ACCEPTED_TERMS_URL' : 'ql:launch', # only in QA
 }
 
 DATABASE_ROUTERS = ['icommons_ext_tools.routers.DatabaseAppsRouter', ]
@@ -37,8 +37,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.oracle',
         'NAME': 'isitedgd',
-        'USER': SECURE_SETTINGS['django_db_user'],
-        'PASSWORD': SECURE_SETTINGS['django_db_pass'],
+        'USER': SECURE_SETTINGS.get('django_db_user'),
+        'PASSWORD': SECURE_SETTINGS.get('django_db_pass'),
         'HOST': 'icd3.isites.harvard.edu',
         'PORT': '8103',
         'OPTIONS': {
@@ -51,7 +51,7 @@ DATABASES = {
 # need to override the NLS_DATE_FORMAT that is set by oraclepool
 '''
 DATABASE_EXTRAS = {
-    'session': ["ALTER SESSION SET NLS_DATE_FORMAT = 'YYYY-MM-DD HH24:MI:SS' NLS_TIMESTAMP_FORMAT = 'YYYY-MM-DD HH24:MI:SS.FF'", ],
+    'session': ["ALTER SESSION SET NLS_DATE_FORMAT = 'YYYY-MM-DD HH24:MI:SS' NLS_TIMESTAMP_FORMAT = 'YYYY-MM-DD HH24:MI:SS.FF'", ),
     'threaded': True,
 }
 '''
@@ -98,7 +98,7 @@ LOGGING = {
     'handlers': {
         'mail_admins': {
             'level': 'ERROR',
-            'filters': ['require_debug_false'],
+            'filters': ['require_debug_false'),
             'class': 'django.utils.log.AdminEmailHandler'
         },
         # Log to a text file that can be rotated by logrotate
@@ -115,52 +115,52 @@ LOGGING = {
     },
     'loggers': {
         'django.request': {
-            'handlers': ['console', 'logfile'],
+            'handlers': ['console', 'logfile'),
             'level': 'ERROR',
             'propagate': True,
         },
         'django': {
-            'handlers': ['console', 'logfile'],
+            'handlers': ['console', 'logfile'),
             'level': 'DEBUG',
             'propagate': True,
         },
         'qualtrics_link': {
-            'handlers': ['console', 'logfile'],
+            'handlers': ['console', 'logfile'),
             'level': 'DEBUG',
             'propagate': True,
         },
         'icommons_common': {
-            'handlers': ['mail_admins', 'console', 'logfile'],
+            'handlers': ['mail_admins', 'console', 'logfile'),
             'level': 'DEBUG',
             'propagate': True,
         },
         'oraclepool': {
-            'handlers': ['console', 'logfile'],
+            'handlers': ['console', 'logfile'),
             'level': 'DEBUG',
             'propagate': True,
         },
         'huey.consumer': {
-            'handlers': ['logfile'],
+            'handlers': ['logfile'),
             'level': 'INFO',
             'propagate': True,
         },
         'icommons_common.auth.views': {
-            'handlers': ['console', 'logfile'],
+            'handlers': ['console', 'logfile'),
             'level': 'DEBUG',
             'propagate': True,
         },
         'rest_framework': {
-            'handlers': ['console', 'logfile'],
+            'handlers': ['console', 'logfile'),
             'level': 'DEBUG',
             'propagate': True,
         },
         'django.db.backends.oracle': {
-            'handlers': ['console', 'logfile'],
+            'handlers': ['console', 'logfile'),
             'level': 'DEBUG',
             'propagate': True,
         },
         'canvas_shopping': {
-            'handlers': ['mail_admins', 'console', 'logfile', ],
+            'handlers': ['mail_admins', 'console', 'logfile', ),
             'level': 'DEBUG',
             'propagate': True,
         },
