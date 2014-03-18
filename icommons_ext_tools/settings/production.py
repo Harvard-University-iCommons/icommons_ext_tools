3612c84146d8893b1254d181147e112eadbf6468
Parker, Eric P <eric_parker@harvard.edu>
2014-03-18 10:39:01 -0400

    added production.py file

---
+++ icommons_ext_tools/settings/production.py
@@ -0,0 +1,173 @@
+
+from .base import *
+
+import os
+
+os.environ['http_proxy'] = 'http://10.34.5.254:8080'
+os.environ['https_proxy'] = 'http://10.34.5.254:8080'
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
+
+ICOMMONS_COMMON = {
+    'ICOMMONS_API_HOST': 'https://isites.harvard.edu/services/',
+    'ICOMMONS_API_USER': SECURE_SETTINGS['icommons_api_user'],
+    'ICOMMONS_API_PASS': SECURE_SETTINGS['icommons_api_pass'],
+}
+
+QUALTRICS_LINK = {
+    'AGREEMENT_ID' : SECURE_SETTINGS['qualtrics_agreement_id'],
+    'QUALTRICS_APP_KEY' : SECURE_SETTINGS['qualtrics_app_key'],
+    'QUALTRICS_API_URL' : SECURE_SETTINGS['qualtrics_api_url'], 
+    'QUALTRICS_API_USER' : SECURE_SETTINGS['qualtrics_api_user'], 
+    'QUALTRICS_API_TOKEN' : SECURE_SETTINGS['qualtrics_api_token'], 
+    'QUALTRICS_AUTH_GROUP' : SECURE_SETTINGS['qualtrics_auth_group'],
+    'USER_DECLINED_TERMS_URL' : 'ql:internal', # only in QA
+    'USER_ACCEPTED_TERMS_URL' : 'ql:internal', # only in QA
+}
+
+DATABASE_ROUTERS = ['icommons_ext_tools.routers.DatabaseAppsRouter', ]
+
+DATABASE_APPS_MAPPING = {
+    'qualtrics_link': 'default',
+}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': 'isiteqa',
+        'USER': SECURE_SETTINGS['django_db_user'],
+        'PASSWORD': SECURE_SETTINGS['django_db_pass'],
+        'HOST': 'icd3.isites.harvard.edu',
+        'PORT': '8003',
+        'OPTIONS': {
+            'threaded': True,
+        },
+        'CONN_MAX_AGE': 0,
+    }
+}
+
+# need to override the NLS_DATE_FORMAT that is set by oraclepool
+'''
+DATABASE_EXTRAS = {
+    'session': ["ALTER SESSION SET NLS_DATE_FORMAT = 'YYYY-MM-DD HH24:MI:SS' NLS_TIMESTAMP_FORMAT = 'YYYY-MM-DD HH24:MI:SS.FF'", ],
+    'threaded': True,
+}
+'''
+
+INSTALLED_APPS += ('gunicorn',)
+
+#CACHES = {
+#    'default': {
+#        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+#    }
+#}
+
+CACHES = {
+    'default': {
+        'BACKEND': 'redis_cache.RedisCache',
+        'LOCATION': '127.0.0.1:6379',
+        'OPTIONS': {
+            'PARSER_CLASS': 'redis.connection.HiredisParser'
+        },
+    },
+}
+
+SESSION_ENGINE = 'redis_sessions.session'
+SESSION_REDIS_HOST = 'localhost'
+SESSION_REDIS_PORT = 6379
+#
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(module)s %(message)s'
+        }
+    },
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        },
+        # Log to a text file that can be rotated by logrotate
+        'logfile': {
+            'class': 'logging.handlers.WatchedFileHandler',
+            'filename': join(SITE_ROOT, 'logs/icommons_ext_tools.log'),
+            'formatter': 'verbose'
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['console', 'logfile'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+        'django': {
+            'handlers': ['console', 'logfile'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+        'qualtrics_link': {
+            'handlers': ['console', 'logfile'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+        'icommons_common': {
+            'handlers': ['mail_admins', 'console', 'logfile'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+        'oraclepool': {
+            'handlers': ['console', 'logfile'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+        'huey.consumer': {
+            'handlers': ['logfile'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+        'icommons_common.auth.views': {
+            'handlers': ['console', 'logfile'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+        'rest_framework': {
+            'handlers': ['console', 'logfile'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+        'django.db.backends.oracle': {
+            'handlers': ['console', 'logfile'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+        'canvas_shopping': {
+            'handlers': ['mail_admins', 'console', 'logfile', ],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+
+    }
+}
+
+SESSION_COOKIE_SECURE = True
+
+GUNICORN_CONFIG = 'gunicorn_prod.py'
