3b6a0a15e5f2f31a53c7022b726b0721ec720245
Parker, Eric P <eric_parker@harvard.edu>
2014-04-18 07:50:49 -0400

    updated sql

--- templates.sql
+++ templates.sql
@@ -41,17 +41,5 @@ ALTER TABLE templates ADD CONSTRAINT templates_PK PRIMARY KEY ( "TEMPLATE_ID" ) 
 
 CREATE SEQUENCE  templates_sq  MINVALUE 1 INCREMENT BY 1 START WITH 1 CACHE 20 ;
 
--- insert into templates values (templates_sq.nextval, 'Fall 2014', 'DCE / Fall 2014', 4580);
--- insert into templates values (templates_sq.nextval, 'Spring 2014', 'Harvard Extension School / Spring 2014', 4580);
--- insert into templates values (templates_sq.nextval, 'Full Year 2014', 'FAS Full Year 2014', 4580);
--- insert into templates values (templates_sq.nextval, 'Fall 2014', 'GSE / Fall 2014', 4580);
--- insert into templates values (templates_sq.nextval, 'Spring 2014', 'GSE / Spring 2014', 4580);
--- insert into templates values (templates_sq.nextval, 'Fall 2014', 'HLS / Fall 2014', 4580);
--- insert into template_accounts values('colgsag','colgsas',  'Y');
--- insert into template_accounts values('ext','Harvard Extension School',  'N');
--- insert into template_accounts values('hds','Harvard Design School',  'Y');
--- insert into template_accounts values('gse','gse',  'Y');
--- insert into template_accounts values('hls-berkman','hls-berkman',  'N');
--- insert into template_users values('20533064', 'ext');
--- insert into template_access_list values('ext',1,'20533064');
+
 
