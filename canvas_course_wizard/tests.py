bfc2223487b2f4866d1adb2769d3338158ee8f26
Bermudez, Jaime <jaime_bermudez@harvard.edu>
2014-05-09 14:57:39 -0400

    Unit testing for CourseWizardIndexView - test url routing and view

--- canvas_course_wizard/tests.py
+++ canvas_course_wizard/tests.py
@@ -1,21 +1,26 @@
 import unittest
-import mock
+#import mock
 from .views import CourseWizardIndexView
+from django.core.urlresolvers import reverse, resolve
+#from django.test import RequestFactory
 # Create your tests here.
 
 
-class CustomViewTest(unittest.TestCase):
-    def test_template_call(self):
-        view = CourseWizardIndexView.as_view()
-        request = mock.MagicMock(name='request')
-        request.method.lower.return_value = 'get'
-        # mock_view.request.user.is_authenticated = lambda: True
-        # mock_view.request.method.lower = lambda: u'get'
-        #view.response_class = mock.Mock(name='response_class')
-        print request.method.lower()
-        response = view(request, None, None)
-        response.render()
-        print request.mock_calls
-        #print view.response_class.mock_calls[0]
+class CourseWizardIndexViewTest(unittest.TestCase):
+
+    def test_route_by_name(self):
+        url = reverse('crs-wiz:wizard-index')
+        self.assertEqual(url, '/ext_tools/course_wizard/')
+
+    def test_route_to_view(self):
+        match = resolve('/ext_tools/course_wizard/')
+        self.assertEquals('CourseWizardIndexView', match.func.__name__)
+        self.assertTrue(len(match.args) == 0, 'Should be no args for course wizard index view')
+        self.assertTrue(len(match.kwargs) == 0, 'Should be no kwargs for course wizard index view')
+
+    def test_template_setup(self):
+        view = CourseWizardIndexView()
+        self.assertTrue(view.template_name)
+        self.assertEquals(view.template_name, 'canvas_course_wizard/index.html')
 
 # Create your tests here.
