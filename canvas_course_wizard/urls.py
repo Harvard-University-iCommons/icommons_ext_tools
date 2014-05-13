31c06625cb4267c4a27fc9c82c61fa341bed8f97
Bermudez, Jaime <jaime_bermudez@harvard.edu>
2014-05-13 14:13:41 -0400

    Add new route and view for course catalog.  The course catalog view will render a catalog template if the course instance was found, otherwise it renders a "course not found" template.  Some initial tests in place for both views.

--- canvas_course_wizard/urls.py
+++ canvas_course_wizard/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, url
 
-from .views import CourseWizardIndexView
+from .views import CourseWizardIndexView, CourseCatalogIndexView
 
 urlpatterns = patterns(
     '',
     url(r'^$', CourseWizardIndexView.as_view(), name='wizard-index'),
+    url(r'^catalog/(?P<school>\w+)/(?P<registrar_code>[-\w]+)/(?P<year>\d{4})/(?P<term>\w+)$',
+        CourseCatalogIndexView.as_view(), name='course-catalog'),
 )
--- canvas_course_wizard/views.py
+++ canvas_course_wizard/views.py
@@ -1,8 +1,10 @@
 from django.views.generic.base import TemplateView
 from braces.views import LoginRequiredMixin
+from icommons_common.models import CourseInstance
 # from braces.views import CsrfExemptMixin
 # from django.http import HttpResponse
 import logging
+from django.core.exceptions import ObjectDoesNotExist
 # import hashlib # Hash encrypt the user's HUID
 # import json # Formats form post that user submitted to Piazza
 # Get an instance of a logger
@@ -15,4 +17,21 @@ logger = logging.getLogger(__name__)
 class CourseWizardIndexView(LoginRequiredMixin, TemplateView):
     template_name = "canvas_course_wizard/index.html"
 
-    
+
+class CourseCatalogIndexView(TemplateView):
+    template_name = 'canvas_course_wizard/course_catalog.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(CourseCatalogIndexView, self).get_context_data(**kwargs)
+        try:
+            selected_course = CourseInstance.objects.get(
+                course__registrar_code=self.kwargs.get('registrar_code', None),
+                term__school_id=self.kwargs.get('school', None),
+                term__academic_year=self.kwargs.get('year', None),
+                term__term_code__term_name__startswith=self.kwargs.get('term', None)
+            )
+            context['course'] = selected_course
+        except ObjectDoesNotExist:
+            self.template_name = 'canvas_course_wizard/course_not_found.html'
+
+        return context
