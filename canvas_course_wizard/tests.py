5ffd3eff25f45bb0407397c579806d45981c3b2e
Bermudez, Jaime <jaime_bermudez@harvard.edu>
2014-05-15 14:37:30 -0400

    Intentionally triggering a test error to see how Jenkins responds.

--- canvas_course_wizard/tests.py
+++ canvas_course_wizard/tests.py
@@ -1,6 +1,6 @@
 import unittest
 import mock
-from .views import CourseWizardIndexView, CourseCatalogIndexView
+from .views import CourseWizardIndexView, CourseIndexView
 from django.core.urlresolvers import reverse, resolve
 from django.views.generic import TemplateView
 from braces.views import LoginRequiredMixin
@@ -8,7 +8,6 @@ from django.test import RequestFactory
 from django.core.urlresolvers import NoReverseMatch
 from icommons_common.models import CourseInstance
 from django.shortcuts import render
-from django.core.exceptions import ObjectDoesNotExist
 
 
 class CourseWizardIndexViewTest(unittest.TestCase):
@@ -16,7 +15,7 @@ class CourseWizardIndexViewTest(unittest.TestCase):
 
     def test_route_to_view(self):
         match = resolve('/', 'canvas_course_wizard.urls')
-        self.assertEqual(match.view_name, 'wizard-index', "named-url for wizard index didn't match expected result")
+        self.assertEqual(match.view_name, 'wizard-indx', "named-url for wizard index didn't match expected result")
         self.assertEqual('CourseWizardIndexView', match.func.__name__)
         self.assertEqual(len(match.args), 0, 'Should be no args for course wizard index view')
         self.assertEqual(len(match.kwargs), 0, 'Should be no kwargs for course wizard index view')
@@ -34,91 +33,99 @@ class CourseWizardIndexViewTest(unittest.TestCase):
         self.assertEquals(result.status_code, 200, 'Wizard index view should render successfully')
             
 
-class CourseCatalogIndexViewTest(unittest.TestCase):
+# class CourseCatalogIndexViewTest(unittest.TestCase):
 
-    def setUp(self):
-        self.factory = RequestFactory()
+#     def setUp(self):
+#         self.request = RequestFactory().get('/fake-path')
 
-    def test_route_by_name(self):
-        url = reverse(
-            'crs-wiz:course-catalog',
-            kwargs={'school': 'ext', 'registrar_code': 21197, 'year': 2014, 'term': 'Fall'}
-        )
-        self.assertEqual(url, '/ext_tools/course_wizard/catalog/ext/21197/2014/Fall')
+#     def test_route_by_name(self):
+#         url = reverse(
+#             'crs-wiz:course-catalog',
+#             kwargs={'school': 'ext', 'registrar_code': 21197, 'year': 2014, 'term': 'Fall'}
+#         )
+#         self.assertEqual(url, '/ext_tools/course_wizard/catalog/ext/21197/2014/Fall')
 
-    def test_route_not_enough_args(self):
-        self.assertRaises(
-            NoReverseMatch,
-            reverse,
-            'crs-wiz:course-catalog',
-            kwargs={'school': 'ext', 'registrar_code': 21197}
-        )
+#     def test_route_not_enough_args(self):
+#         self.assertRaises(
+#             NoReverseMatch,
+#             reverse,
+#             'crs-wiz:course-catalog',
+#             kwargs={'school': 'ext', 'registrar_code': 21197}
+#         )
 
-    def test_route_invalid_year_kwarg(self):
-        self.assertRaises(
-            NoReverseMatch,
-            reverse,
-            'crs-wiz:course-catalog',
-            kwargs={'school': 'ext', 'registrar_code': '21197', 'year': 14, 'term': 'Fall'}
-        )
+#     def test_route_invalid_year_kwarg(self):
+#         self.assertRaises(
+#             NoReverseMatch,
+#             reverse,
+#             'crs-wiz:course-catalog',
+#             kwargs={'school': 'ext', 'registrar_code': '21197', 'year': 14, 'term': 'Fall'}
+#         )
 
-    def test_route_to_view(self):
-        match = resolve('/ext_tools/course_wizard/catalog/ext/21197/2014/Fall')
-        self.assertEquals(match.view_name, 'crs-wiz:course-catalog')
-        self.assertEquals(CourseCatalogIndexView.__name__, match.func.__name__)
-        self.assertTrue(len(match.args) == 0, 'Should be no args for course wizard index view')
-        self.assertEquals(len(match.kwargs), 4)
-        self.assertEquals(match.kwargs['school'], 'ext')
-        self.assertEquals(match.kwargs['registrar_code'], '21197')
-        self.assertEquals(match.kwargs['year'], '2014')
-        self.assertEquals(match.kwargs['term'], 'Fall')
+#     def test_route_to_view(self):
+#         match = resolve('/ext_tools/course_wizard/catalog/ext/21197/2014/Fall')
+#         self.assertEquals(match.view_name, 'crs-wiz:course-catalog')
+#         self.assertEquals(CourseCatalogIndexView.__name__, match.func.__name__)
+#         self.assertTrue(len(match.args) == 0, 'Should be no args for course wizard index view')
+#         self.assertEquals(len(match.kwargs), 4)
+#         self.assertEquals(match.kwargs['school'], 'ext')
+#         self.assertEquals(match.kwargs['registrar_code'], '21197')
+#         self.assertEquals(match.kwargs['year'], '2014')
+#         self.assertEquals(match.kwargs['term'], 'Fall')
 
-    def test_get_context_data_for_existing_course(self):
-        selected_course = CourseInstance()
-        mock_course_instance = mock.Mock(name='course-instance')
-        mock_course_instance.objects.get.return_value = selected_course
+#     def test_get_context_data_for_existing_course(self):
+#         selected_course = CourseInstance()
+#         # For this method, self.object is expected to be an attribute so it's being defaulted to None here.  In
+#         # Django 1.7 branch the assumption is now conditionalized.
+#         view = CourseCatalogIndexView()
+#         view.request = self.request
+#         view.kwargs = {'school': 'ext', 'registrar_code': '21197', 'year': 2014, 'term': 'Fall'}
 
-        request = self.factory.get('/fake-path')
-        # For this method, self.object is expected to be an attribute so it's being defaulted to None here.  In
-        # Django 1.7 branch the assumption is now conditionalized.
-        view = CourseCatalogIndexView()
-        view.request = request
-        view.kwargs = {'school': 'ext', 'registrar_code': '21197', 'year': 2014, 'term': 'Fall'}
+#         with mock.patch('canvas_course_wizard.views.CourseInstance.objects.get') as mock_db_course_get:
+#             with mock.patch('canvas_course_wizard.views.super', create=True) as mock_super:
+#                 # Set up the mocks
+#                 mock_db_course_get.return_value = selected_course
+#                 mock_super.return_value.get_context_data.return_value = {}
+#                 # Make the call
+#                 context = view.get_context_data()
 
-        with mock.patch('canvas_course_wizard.views.CourseInstance', mock_course_instance):
-            context = view.get_context_data()
+#         self.assertEquals(context['course'], selected_course)
+#         mock_db_course_get.assert_called_once_with(
+#             course__registrar_code='21197',
+#             term__school_id='ext',
+#             term__academic_year=2014,
+#             term__term_code__term_name__startswith='Fall'
+#         )
+#         self.assertEquals(view.template_name, 'canvas_course_wizard/course_catalog.html')
 
-        self.assertEquals(context['course'], selected_course)
-        mock_course_instance.objects.get.assert_called_once_with(
-            course__registrar_code='21197',
-            term__school_id='ext',
-            term__academic_year=2014,
-            term__term_code__term_name__startswith='Fall'
-        )
-        self.assertEquals(view.template_name, 'canvas_course_wizard/course_catalog.html')
+#     def test_get_context_data_for_non_existing_course(self):
+#         # For this method, self.object is expected to be an attribute so it's being defaulted to None here.  In
+#         # Django 1.7 branch the assumption is now conditionalized.
+#         view = CourseCatalogIndexView()
+#         view.request = self.request
+#         view.kwargs = {'school': 'ext', 'registrar_code': '21197', 'year': 2014, 'term': 'Fall'}
 
-    def test_get_context_data_for_non_existing_course(self):
-        mock_course_instance = mock.Mock(name='course-instance')
-        mock_course_instance.objects.get.side_effect = ObjectDoesNotExist('blah')
+#         with mock.patch('canvas_course_wizard.views.CourseInstance.objects.get') as mock_db_course_get:
+#             with mock.patch('canvas_course_wizard.views.super', create=True) as mock_super:
 
-        request = self.factory.get('/fake-path')
-        # For this method, self.object is expected to be an attribute so it's being defaulted to None here.  In
-        # Django 1.7 branch the assumption is now conditionalized.
-        view = CourseCatalogIndexView()
-        view.request = request
-        view.kwargs = {'school': 'ext', 'registrar_code': 'bogus', 'year': 1999, 'term': 'Fall'}
+#                 mock_db_course_get.side_effect = CourseInstance.DoesNotExist
+#                 mock_super.return_value.get_context_data.return_value = {}
+#                 context = view.get_context_data()
 
-        with mock.patch('canvas_course_wizard.views.CourseInstance', mock_course_instance):
-            context = view.get_context_data()
-            # mock_course_instance.objects.get.assert_called_once_with(
-            #     course__registrar_code='bogus',
-            #     term__school_id='ext',
-            #     term__academic_year=1999,
-            #     term__term_code__term_name__startswith='Fall'
-            # )
-        #print context
-        #self.assertEquals(len(context), 0)
-        self.assertEquals(view.template_name, 'canvas_course_wizard/course_not_found.html')
+#         self.assertEquals(len(context), 0)
+#         mock_db_course_get.assert_called_once_with(
+#             course__registrar_code='21197',
+#             term__school_id='ext',
+#             term__academic_year=2014,
+#             term__term_code__term_name__startswith='Fall'
+#         )
+#         self.assertEquals(view.template_name, 'canvas_course_wizard/course_not_found.html')
 
+#     def test_render_catalog_template(self):
+#         template_name = 'canvas_course_wizard/course_catalog.html'
+#         result = render(self.request, template_name)
+#         self.assertEquals(result.status_code, 200)
 
-# Create your tests here.
+#     def test_render_course_not_found_template(self):
+#         template_name = 'canvas_course_wizard/course_not_found.html'
+#         result = render(self.request, template_name)
+#         self.assertEquals(result.status_code, 200)
