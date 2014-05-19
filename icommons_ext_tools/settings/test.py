eeeccaa1464a4603ef25d34b0008223f31a0f738
Parker, Eric P <eric_parker@harvard.edu>
2014-05-19 14:20:58 -0400

    changed settings to allow code to execute on test as well as run tests on jenkins

--- icommons_ext_tools/settings/test.py
+++ icommons_ext_tools/settings/test.py
@@ -51,14 +51,14 @@ DATABASES = {
 }
 
 # make tests faster
-SOUTH_TESTS_MIGRATE = False
-DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': os.path.join(os.path.dirname(__file__), 'test.db'),
-       'TEST_NAME': os.path.join(os.path.dirname(__file__), 'test.db'),
-   },
-}
+# SOUTH_TESTS_MIGRATE = False
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(os.path.dirname(__file__), 'test.db'),
+#        'TEST_NAME': os.path.join(os.path.dirname(__file__), 'test.db'),
+#    },
+# }
 
 # need to override the NLS_DATE_FORMAT that is set by oraclepool
 '''
