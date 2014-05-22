785a017bba92e381569f30f3a33ec2cc23f91218
Bermudez, Jaime <jaime_bermudez@harvard.edu>
2014-05-22 15:02:25 -0400

    Tests for Custom404Mixin.  Some other pep8 fixes in same file.

--- canvas_course_wizard/tests.py
+++ canvas_course_wizard/tests.py
@@ -1,6 +1,7 @@
 import unittest
 import mock
 from .views import CourseWizardIndexView, CourseIndexView
+from .mixins import Custom404Mixin
 from django.core.urlresolvers import resolve
 from django.views.generic import TemplateView
 from django.views.generic.detail import DetailView
@@ -8,6 +9,52 @@ from braces.views import LoginRequiredMixin
 from django.test import RequestFactory
 from icommons_common.models import CourseInstance
 from django.shortcuts import render
+from django.core.exceptions import ImproperlyConfigured
+from django.http import Http404
+
+
+class Custom404MixinTest(unittest.TestCase):
+    longMessage = True
+
+    def setUp(self):
+        self.mixin = Custom404Mixin()
+
+    def test_instance_setup(self):
+        self.assertEqual(self.mixin.template_name_404, None,
+                         "Custom404Mixin should have default template name attribute set to None")
+
+    def test_get_404_template_names_none_template_raises_improperly_configured(self):
+        with self.assertRaises(ImproperlyConfigured):
+            self.mixin.get_404_template_names()
+
+    def test_get_404_template_names_result_matches_template_attribute(self):
+        template_name_404 = 'canvas_course_wizard/my_custom_404_page.html'
+        self.mixin.template_name_404 = template_name_404
+        self.assertEqual(self.mixin.get_404_template_names(), [template_name_404],
+                         "Template result should be a list containing defined class attribute")
+
+    @mock.patch('canvas_course_wizard.mixins.super', create=True)
+    @mock.patch('canvas_course_wizard.mixins.TemplateResponse')
+    def test_dispatch_response_when_404_raised(self, template_response_mock, super_mock):
+        request = RequestFactory().get('/fake-path')
+        get_404_template_names_mock = mock.Mock()
+        # Ensure call to super.dispatch raises a 404
+        super_mock.return_value.dispatch.side_effect = Http404
+        self.mixin.get_404_template_names = get_404_template_names_mock
+        response = self.mixin.dispatch(request)
+        self.assertEqual(response, template_response_mock,
+                         "Return value of dispatch when 404 is raised should be a TemplateResponse object")
+        get_404_template_names_mock.assert_called_once_with()
+        template_response_mock.assert_called_with(status=404, request=request, template=mock.ANY)
+
+    @mock.patch('canvas_course_wizard.mixins.super', create=True)
+    def test_dispatch_response_when_404_not_raised(self, super_mock):
+        request = RequestFactory().get('/fake-path')
+        response = self.mixin.dispatch(request)
+        # Little trick here to ensure that a call to the dispatch method normally
+        # returns the result of super.dispatch
+        self.assertEqual(response, super_mock.return_value.dispatch.return_value,
+                         "Call to dispatch that doesn't raise an exception should return result super().dispatch()")
 
 
 class CourseWizardIndexViewTest(unittest.TestCase):
@@ -15,15 +62,18 @@ class CourseWizardIndexViewTest(unittest.TestCase):
 
     def test_resolve_route_to_view(self):
         match = resolve('/', 'canvas_course_wizard.urls')
-        self.assertEqual(match.view_name, 'wizard-index', "named-url for wizard index didn't match expected result")
+        self.assertEqual(match.view_name, 'wizard-index',
+                         "named-url for wizard index didn't match expected result")
         self.assertEqual('CourseWizardIndexView', match.func.__name__)
         self.assertEqual(len(match.args), 0, 'Should be no args for course wizard index view')
         self.assertEqual(len(match.kwargs), 0, 'Should be no kwargs for course wizard index view')
 
     def test_view_instance_setup(self):
         view = CourseWizardIndexView()
-        self.assertIsInstance(view, TemplateView, 'Wizard index view should be a subclass of TemplateView')
-        self.assertIsInstance(view, LoginRequiredMixin, 'Wizard index view should implement the LoginRequiredMixin')
+        self.assertIsInstance(view, TemplateView,
+                              'Wizard index view should be a subclass of TemplateView')
+        self.assertIsInstance(view, LoginRequiredMixin,
+                              'Wizard index view should implement the LoginRequiredMixin')
         self.assertEquals(view.template_name, 'canvas_course_wizard/index.html')
 
     def test_template_render(self):
@@ -31,7 +81,7 @@ class CourseWizardIndexViewTest(unittest.TestCase):
         template_name = 'canvas_course_wizard/index.html'
         result = render(request, template_name)
         self.assertEquals(result.status_code, 200, 'Wizard index view should render successfully')
-            
+
 
 class CourseIndexViewTest(unittest.TestCase):
     longMessage = True
@@ -41,23 +91,28 @@ class CourseIndexViewTest(unittest.TestCase):
 
     def test_resolve_route_to_view(self):
         match = resolve('/courses/123/', 'canvas_course_wizard.urls')
-        self.assertEqual(match.view_name, 'course-index', "named-url for course index didn't match expected result")
+        self.assertEqual(match.view_name, 'course-index',
+                         "named-url for course index didn't match expected result")
         self.assertEqual('CourseIndexView', match.func.__name__)
         self.assertEqual(len(match.args), 0, 'Should be no args for course wizard index view')
         self.assertEqual(len(match.kwargs), 1, 'Should be one keyword argument for course index')
-        self.assertEqual(match.kwargs['pk'], '123', 'Keyword argument \'pk\' should match end of url')
+        self.assertEqual(match.kwargs['pk'], '123',
+                         'Keyword argument \'pk\' should match end of url')
 
     def test_view_instance_setup(self):
         view = CourseIndexView()
-        self.assertIsInstance(view, DetailView, 'Wizard index view expected to be a subclass of DetailView')
+        self.assertIsInstance(view, DetailView,
+                              'Wizard index view expected to be a subclass of DetailView')
         self.assertEquals(view.template_name, 'canvas_course_wizard/course.html')
         self.assertIs(view.model, CourseInstance, 'Model lookup type should be CourseInstance')
-        self.assertEqual(view.context_object_name, 'course', "context should reference the course instance as 'course'")
+        self.assertEqual(view.context_object_name, 'course',
+                         "context should reference the course instance as 'course'")
 
     '''
     Expectation entering the get_context_data is that the object has been found... a 404 error would have been
     raised before getting to this method, so we'll do the minimal setup required
     '''
+
     def test_get_context_data_for_non_canvas_course_as_teaching_staff(self):
 
         # Make sure user is considered a staff member
@@ -77,11 +132,12 @@ class CourseIndexViewTest(unittest.TestCase):
             context = view.get_context_data()
 
         mock_is_staffer.assert_called_once_with(mock_ci.course_instance_id)
-        self.assertTrue(context['show_create'], 'A teaching staffer for a non-canvas course should be able to create')
+        self.assertTrue(context['show_create'],
+                        'A teaching staffer for a non-canvas course should be able to create')
 
     # def test_get_context_data_for_non_existing_course(self):
-    #     # For this method, self.object is expected to be an attribute so it's being defaulted to None here.  In
-    #     # Django 1.7 branch the assumption is now conditionalized.
+    # For this method, self.object is expected to be an attribute so it's being defaulted to None here.  In
+    # Django 1.7 branch the assumption is now conditionalized.
     #     view = CourseCatalogIndexView()
     #     view.request = self.request
     #     view.kwargs = {'school': 'ext', 'registrar_code': '21197', 'year': 2014, 'term': 'Fall'}
