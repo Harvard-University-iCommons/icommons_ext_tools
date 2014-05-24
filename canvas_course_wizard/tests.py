0dfa01d459cd811f004fb8d0e21c534b92975159
Parker, Eric P <eric_parker@harvard.edu>
2014-05-24 16:42:37 -0400

    Added functionality to the CourseIndexView to see if an isite or canvas site already exists for the given course_instance_id.

--- canvas_course_wizard/tests.py
+++ canvas_course_wizard/tests.py
@@ -7,8 +7,9 @@ from django.views.generic.detail import DetailView
 from braces.views import LoginRequiredMixin
 from django.test import RequestFactory
 from django.core.urlresolvers import NoReverseMatch
-from icommons_common.models import CourseInstance
+from icommons_common.models import CourseInstance, SiteMap, CourseSite
 from django.shortcuts import render
+from django.conf import settings
 
 
 class CourseWizardIndexViewTest(unittest.TestCase):
@@ -16,15 +17,18 @@ class CourseWizardIndexViewTest(unittest.TestCase):
 
     def test_resolve_route_to_view(self):
         match = resolve('/', 'canvas_course_wizard.urls')
-        self.assertEqual(match.view_name, 'wizard-index', "named-url for wizard index didn't match expected result")
+        self.assertEqual(match.view_name, 'wizard-index',
+                         "named-url for wizard index didn't match expected result")
         self.assertEqual('CourseWizardIndexView', match.func.__name__)
         self.assertEqual(len(match.args), 0, 'Should be no args for course wizard index view')
         self.assertEqual(len(match.kwargs), 0, 'Should be no kwargs for course wizard index view')
 
     def test_view_instance_setup(self):
         view = CourseWizardIndexView()
-        self.assertIsInstance(view, TemplateView, 'Wizard index view should be a subclass of TemplateView')
-        self.assertIsInstance(view, LoginRequiredMixin, 'Wizard index view should implement the LoginRequiredMixin')
+        self.assertIsInstance(
+            view, TemplateView, 'Wizard index view should be a subclass of TemplateView')
+        self.assertIsInstance(
+            view, LoginRequiredMixin, 'Wizard index view should implement the LoginRequiredMixin')
         self.assertEquals(view.template_name, 'canvas_course_wizard/index.html')
 
     def test_template_render(self):
@@ -32,7 +36,7 @@ class CourseWizardIndexViewTest(unittest.TestCase):
         template_name = 'canvas_course_wizard/index.html'
         result = render(request, template_name)
         self.assertEquals(result.status_code, 200, 'Wizard index view should render successfully')
-            
+
 
 class CourseIndexViewTest(unittest.TestCase):
     longMessage = True
@@ -42,23 +46,28 @@ class CourseIndexViewTest(unittest.TestCase):
 
     def test_resolve_route_to_view(self):
         match = resolve('/courses/123/', 'canvas_course_wizard.urls')
-        self.assertEqual(match.view_name, 'course-index', "named-url for course index didn't match expected result")
+        self.assertEqual(match.view_name, 'course-index',
+                         "named-url for course index didn't match expected result")
         self.assertEqual('CourseIndexView', match.func.__name__)
         self.assertEqual(len(match.args), 0, 'Should be no args for course wizard index view')
         self.assertEqual(len(match.kwargs), 1, 'Should be one keyword argument for course index')
-        self.assertEqual(match.kwargs['pk'], '123', 'Keyword argument \'pk\' should match end of url')
+        self.assertEqual(
+            match.kwargs['pk'], '123', 'Keyword argument \'pk\' should match end of url')
 
     def test_view_instance_setup(self):
         view = CourseIndexView()
-        self.assertIsInstance(view, DetailView, 'Wizard index view expected to be a subclass of DetailView')
+        self.assertIsInstance(
+            view, DetailView, 'Wizard index view expected to be a subclass of DetailView')
         self.assertEquals(view.template_name, 'canvas_course_wizard/course.html')
         self.assertIs(view.model, CourseInstance, 'Model lookup type should be CourseInstance')
-        self.assertEqual(view.context_object_name, 'course', "context should reference the course instance as 'course'")
+        self.assertEqual(view.context_object_name, 'course',
+                         "context should reference the course instance as 'course'")
 
     '''
     Expectation entering the get_context_data is that the object has been found... a 404 error would have been
     raised before getting to this method, so we'll do the minimal setup required
     '''
+
     def test_get_context_data_for_non_canvas_course_as_teaching_staff(self):
 
         # Make sure user is considered a staff member
@@ -72,17 +81,69 @@ class CourseIndexViewTest(unittest.TestCase):
         view.is_current_user_member_of_course_staff = mock_is_staffer
 
         with mock.patch('canvas_course_wizard.views.super', create=True) as mock_super:
-            # Set up the patch mocks
-            mock_super.return_value.get_context_data.return_value = {}
-            # Make the call
-            context = view.get_context_data()
+            with mock.patch('canvas_course_wizard.views.SiteMap.objects.get') as mock_sitemap:
+                # Set up the patch mocks
+                mock_super.return_value.get_context_data.return_value = {}
+                # Make the call
+                context = view.get_context_data()
 
         mock_is_staffer.assert_called_once_with(mock_ci.course_instance_id)
-        self.assertTrue(context['show_create'], 'A teaching staffer for a non-canvas course should be able to create')
+        self.assertTrue(
+            context['show_create'], 'A teaching staffer for a non-canvas course should be able to create')
+
+    def test_find_isite_from_course_instance_id(self):
+
+        error_message = 'Given a course instance_id that has an isite setup, the url to the isite should be constructed and returned in the context'
+        # Make sure user is considered a staff member
+        mock_is_staffer = mock.Mock(name='is_staffer', return_value=True)
+        # Course instance to use for test
+        mock_ci = CourseInstance(course_instance_id=9999)
+
+        view = CourseIndexView()
+        view.request = self.request
+        view.object = mock_ci
+        view.is_current_user_member_of_course_staff = mock_is_staffer
+        with mock.patch('canvas_course_wizard.views.super', create=True) as mock_super:
+            with mock.patch('canvas_course_wizard.views.SiteMap.objects.get') as mock_sitemap:
+                # mock_sitemap.return_value
+
+                # set the return value for the mocked database call
+                mock_sitemap().course_site.external_id = 'k12345'
+
+                mock_super.return_value.get_context_data.return_value = {}
+
+                # Make the call
+                context = view.get_context_data()
+
+        self.assertEquals(
+            context['isite_course_url'], 'http://isites.harvard.edu/k12345', error_message)
+
+    # def test_isite_url_or_false(self):
+    #     with mock.patch('canvas_course_wizard.views.SiteMap') as my_model_mock:
+
+    #         mock_ci = CourseInstance(course_instance_id=211144)
+
+    #         view = CourseIndexView()
+    #         view.request = self.request
+    #         view.object = mock_ci
+
+    #         my_model_mock.objects = mock.Mock()
+
+    #         conf = {'get.side_effect': SiteMap.DoesNotExist}
+    #         my_model_mock.objects.configure_mock(**conf)
+    #         self.assertFalse(view.get_isite_url_or_false(211144))
+
+    #         conf = {'get.return_value': mock.Mock()}
+    #         my_model_mock.objects.configure_mock(**conf)
+    #         self.assertTrue(view.get_isite_url_or_false(211144))
+
+    # def test_get_canvas_url_or_false(self):
+    #    pass
+
 
     # def test_get_context_data_for_non_existing_course(self):
-    #     # For this method, self.object is expected to be an attribute so it's being defaulted to None here.  In
-    #     # Django 1.7 branch the assumption is now conditionalized.
+    # For this method, self.object is expected to be an attribute so it's being defaulted to None here.  In
+    # Django 1.7 branch the assumption is now conditionalized.
     #     view = CourseCatalogIndexView()
     #     view.request = self.request
     #     view.kwargs = {'school': 'ext', 'registrar_code': '21197', 'year': 2014, 'term': 'Fall'}
--- canvas_course_wizard/views.py
+++ canvas_course_wizard/views.py
@@ -1,10 +1,14 @@
 from django.views.generic.base import TemplateView
 from django.views.generic.detail import DetailView
+from django.core.exceptions import ObjectDoesNotExist
+from django.conf import settings
 from braces.views import LoginRequiredMixin
-from icommons_common.models import CourseInstance
+from icommons_common.models import CourseInstance, SiteMap
+
 # from braces.views import CsrfExemptMixin
 # from django.http import HttpResponse
 import logging
+
 # import hashlib # Hash encrypt the user's HUID
 # import json # Formats form post that user submitted to Piazza
 # Get an instance of a logger
@@ -16,7 +20,7 @@ logger = logging.getLogger(__name__)
 
 class CourseWizardIndexView(LoginRequiredMixin, TemplateView):
     template_name = "canvas_course_wizard/index.html"
-
+ 
 
 class CourseIndexView(DetailView):
     template_name = 'canvas_course_wizard/course.html'
@@ -28,16 +32,43 @@ class CourseIndexView(DetailView):
         user_groups = self.request.session.get('USER_GROUPS', [])
         return staff_group in user_groups
 
+
     def get_context_data(self, **kwargs):
+        """ 
+        given a course_instance_id determine if an isite exists. 
+        If one does, return the url to the isite. If no, return false.
+        test id for dev 76592 has no iSite, id 21114 has an isite, id 298211 has both 
+        a canvas site and an isite
+
+        given a course_instance_id determine if a canvas site already exists. 
+        If one does, return the url to the site. If no, return false.
+        test id for dev 76592 has no iSite, id 21114 has an isite, id 298211 has both 
+        a canvas site and an isite    
+        """
+
         context = super(CourseIndexView, self).get_context_data(**kwargs)
 
         selected_course = self.object
+        
+        try:
+            site_map = SiteMap.objects.get(course_instance__course_instance_id=selected_course.course_instance_id, 
+                map_type__map_type_id='official')
+    
+            context['isite_course_url'] = settings.COURSE_WIZARD.get('OLD_LMS_URL', None)+site_map.course_site.external_id
+            #
+        except ObjectDoesNotExist:
+            context['isite_course_url'] = None
 
-        # User can create a course if a canvas course does not already exist and if the user is a  member of the teaching staff
+        if selected_course.canvas_course_id:
+            context['canvas_course_url'] = settings.COURSE_WIZARD.get('CANVAS_SERVER_BASE_URL', None) + 'courses/' + str(selected_course.canvas_course_id)
+        else:
+            context['canvas_course_url'] = None
+
+        # User can create a course if a canvas course does not already exist and
+        # if the user is a  member of the teaching staff
         if not selected_course.canvas_course_id and self.is_current_user_member_of_course_staff(selected_course.course_instance_id):
             context['show_create'] = True
         else:
             context['show_create'] = False
-            
+
         return context
-        
