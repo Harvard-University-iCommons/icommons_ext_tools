1316d50373f9b396d3810230583d34553334f06c
Mysore, Sapna <sapna_mysore@harvard.edu>
2014-09-26 13:22:09 -0400

    Using the proxy model and model_api for getting course information

--- canvas_course_creation/controller.py
+++ canvas_course_creation/controller.py
@@ -1,4 +1,5 @@
-from icommons_common.models import CourseInstance
+from .models import CourseData
+from .models_api import get_course_data
 #from canvas_sdk.methods.courses import create_new_course
 from canvas_sdk.methods.sections import create_course_section
 from django.conf import settings
@@ -14,44 +15,42 @@ logger = logging.getLogger(__name__)
 #This method creates a canvas course for the  sis_course_id provided.
 def create_canvas_course(sis_course_id):
 
-	result = None
 	logger.debug("sis id=%s" %sis_course_id)
-	
+	result = None
+
 	try:
 		#1. fetch the course instance info 
 				#Currently using  CourseInstance model: to be replaced by the Proxy model
-		ci = CourseInstance.objects.get(course_instance_id=sis_course_id)
-		if ci:
-			logger.debug(" \nobtained  course info for ci=%s, title=%s, SDK Context=%s"
-			 %(ci,ci.short_title,SDK_CONTEXT))
-			result=ci
+		course_data = get_course_data(sis_course_id)
+		if course_data:
+			logger.debug(" \nobtained  course info for ci=%s, title=%s, section_name=%s"
+			 %(course_data,course_data.short_title, course_data.main_section_name))
+			result=course_data
 
 			# #2. Create canvas course 
 			# new_course = create_new_course(SDK_CONTEXT,
-			# 		account_id=ci.account_id,
-			# 		course_name=ci.course_name,
-			# 		course_course_code=ci.course_code, 
-			# 		course_term_id=ci.term_id,
+			# 		account_id=course_data.account_id,
+			# 		course_name=course_data.course_name,
+			# 		course_course_code=course_data.course_code, 
+			# 		course_term_id=course_data.term_id,
 			# 		course_sis_course_id=sis_course_id)
 			
 			# if new_course:
 
-			#Temporarily reusing the ci object til the SDK call is fixed. The next 2 lines to be removed
-			new_course= ci
+			#Temporarily reusing the course_data in place of new_course object
+			# till the SDK call is fixed. The next 2 lines to be removed
+			new_course= course_data
 			new_course.id='5947'
 
 			#3. Create course section after course  creation
 			section = create_course_section(
 		         		SDK_CONTEXT, 
 		         		course_id=new_course.id,
-		         		course_section_name=ci.short_title
-		         		#course_section_name=ci.section_name
+		         		course_section_name=course_data.main_section_name,
 		         		#course_section_sis_section_id=sis_course_id
 		         		)
 			logger.debug("\n created section")
 
-		   	# else : 
-		   	# 	logger.error('Canvas course object is Null for sis_course_id=%s' %sis_course_id)
 		else:
 			logger.error('No course record  found for %s sis_course_id!' %(sis_course_id) )
 	except Exception as e:
