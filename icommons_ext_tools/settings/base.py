7d7dfbde7e31379b6ee517d3d44d044466ad15b5
Eric Parker <eric_parker@harvard.edu>
2014-10-29 13:30:25 -0400

    added ADMINS constant to base.py, this constant contains a tuple of admin email addresses. Django will send email to admins in this list when a 500 server error occurs. Also added 'django.middleware.common.BrokenLinkEmailsMiddleware' to the middleware classes block, this allows Django to email admins when a 404 occurs. There is also a commented out block called IGNORABLE_404_URLS which can be used to filter out noncritial 404 errors from being emailed. Also added text to the 403, 404, and 500 templates.

--- icommons_ext_tools/settings/base.py
+++ icommons_ext_tools/settings/base.py
@@ -27,7 +27,7 @@ path.append(SITE_ROOT)
 ### End path stuff
 
 # THESE ADDRESSES WILL RECEIVE EMAIL ABOUT CERTAIN ERRORS!
-#ADMINS = SECURE_SETTINGS.get('admins')
+ADMINS = SECURE_SETTINGS.get('admins')
 
 # This is the address that emails will be sent "from"
 SERVER_EMAIL = 'iCommons Tools <icommons-bounces@harvard.edu>'
@@ -112,6 +112,7 @@ TEMPLATE_LOADERS = (
 )
 
 MIDDLEWARE_CLASSES = (
+    'django.middleware.common.BrokenLinkEmailsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -123,7 +124,19 @@ MIDDLEWARE_CLASSES = (
 
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 )
+
+# I've added in this block and commented it out.
+# we may decide we want to add some patterns in here
+# if we start getting to much email from non-critical sources.
+# import re
+# IGNORABLE_404_URLS = (
+#     re.compile(r'^/apple-touch-icon.*\.png$'),
+#     re.compile(r'^/favicon\.ico$'),
+#     re.compile(r'^/robots\.txt$'),
+# )
+
 
 AUTHENTICATION_BACKENDS = (
     'icommons_common.auth.backends.PINAuthBackend',
@@ -149,6 +162,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    SITE_ROOT + '/templates/',
 )
 
 INSTALLED_APPS = (
