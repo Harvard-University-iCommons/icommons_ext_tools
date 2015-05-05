04fb974738aa1de429da78292684013394e9b8b6
Dave Bonner <david_bonner@harvard.edu>
2015-05-05 11:29:59 -0400

    remove canvas_wizard, canvas_course_wizard

--- icommons_ext_tools/urls.py
+++ icommons_ext_tools/urls.py
@@ -1,24 +1,17 @@
 from django.conf.urls import (patterns, include, url)
-
 from django.contrib import admin
-admin.autodiscover()
 
 from canvas_course_site_wizard import urls as ccsw_urls
 
+
+admin.autodiscover()
+
 urlpatterns = patterns(
     '',
-
-    url(r'^ext_tools/qualtrics_link/', include('qualtrics_link.urls', namespace="ql")),
-
-    url(r'^ext_tools/canvas_wizard/', include('canvas_wizard.urls', namespace="cw")),
-
-    url(r'^ext_tools/pin/', include('icommons_common.auth.urls', namespace="pin")),
-
-    url(r'^ext_tools/not_authorized/', 'icommons_ui.views.not_authorized', name="not_authorized"),
-
-    url(r'^ext_tools/course_wizard/', include('canvas_course_wizard.urls', namespace="crs-wiz")),
-
     url(r'^ext_tools/canvas-course-site-wizard/', include(ccsw_urls)),
+    url(r'^ext_tools/not_authorized/', 'icommons_ui.views.not_authorized', name="not_authorized"),
+    url(r'^ext_tools/pin/', include('icommons_common.auth.urls', namespace="pin")),
+    url(r'^ext_tools/qualtrics_link/', include('qualtrics_link.urls', namespace="ql")),
 )
 
 handler403 = 'icommons_ext_tools.views.handler403'
