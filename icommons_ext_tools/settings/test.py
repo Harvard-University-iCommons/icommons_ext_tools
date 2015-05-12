5b21aeb04d151afdf2901c85d390d98c394b262e
Mysore, Sapna <sapna_mysore@harvard.edu>
2015-05-12 14:55:22 -0400

    TLT-1440: Update error e-mail address in non-prod instances to the g.harvard id to avoid opening SNOW tickets in non-prod environments.

--- icommons_ext_tools/settings/test.py
+++ icommons_ext_tools/settings/test.py
@@ -13,6 +13,7 @@ CANVAS_EMAIL_NOTIFICATION['course_migration_success_subject'] += ' (TEST, PLEASE
 CANVAS_EMAIL_NOTIFICATION['course_migration_failure_subject'] += ' (TEST, PLEASE IGNORE)'
 CANVAS_EMAIL_NOTIFICATION['support_email_subject_on_failure'] += ' (TEST, PLEASE IGNORE)'
 CANVAS_EMAIL_NOTIFICATION['environment'] = 'Test'
+CANVAS_EMAIL_NOTIFICATION['support_email_address'] = 'tltqaemails@g.harvard.edu'
 
 COURSE_WIZARD['TERM_TOOL_BASE_URL'] = 'https://test.tlt.harvard.edu'
 
