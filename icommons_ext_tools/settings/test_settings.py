8f167e0124152a2856f5a83db228f8cbdc2551b2
Yates, Elliott <elliott_yates@harvard.edu>
2015-05-19 14:35:08 -0400

    unit tests explicitly do not use database routers

--- icommons_ext_tools/settings/test_settings.py
+++ icommons_ext_tools/settings/test_settings.py
@@ -2,6 +2,8 @@ from .base import *
 
 # NOTE: during tests email is saved in django.core.mail.outbox and no real email is sent
 
+DATABASE_ROUTERS = []
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
