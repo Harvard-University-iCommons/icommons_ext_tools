98eae34914a15e7ef413c353f07ad4d531e299e6
Yates, Elliott <elliott_yates@harvard.edu>
2015-05-19 13:34:08 -0400

    unit test settings do not need a database router

--- icommons_ext_tools/settings/test_settings.py
+++ icommons_ext_tools/settings/test_settings.py
@@ -9,13 +9,4 @@ DATABASES = {
     },
 }
 
-DATABASE_ROUTERS = ['icommons_common.routers.DatabaseAppsRouter']
-DATABASE_APPS_MAPPING = {
-    'canvas_course_site_wizard': 'default',
-    'auth': 'default',
-    'contenttypes': 'default',
-    'sessions': 'default',
-}
-DATABASE_MIGRATION_WHITELIST = ['default']
-
 ISITES_LMS_URL = ''
