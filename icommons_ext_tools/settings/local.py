3ec0e2974bcfb3d5b3aa2f00b414c925d819a31b
Yates, Elliott <elliott_yates@harvard.edu>
2017-01-25 14:33:36 -0500

    tlt-2920: remove django-canvas-course-site-wizard library references

--- icommons_ext_tools/settings/local.py
+++ icommons_ext_tools/settings/local.py
@@ -8,16 +8,6 @@ DEBUG = True
 #  Dummy secret key value for testing and local usage
 SECRET_KEY = "==$(1zr5hus_7r@)g4m^t@0qztiqeeeby5%r20q(sa2i^q@-0k"
 
-# To allow local development server to load static files with DEBUG=False, run:
-#   manage.py runserver --insecure
-# Note: this should never be done for anything but protected local development purposes.
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-CANVAS_EMAIL_NOTIFICATION['course_migration_success_subject'] += ' (TEST, PLEASE IGNORE)'
-CANVAS_EMAIL_NOTIFICATION['course_migration_failure_subject'] += ' (TEST, PLEASE IGNORE)'
-CANVAS_EMAIL_NOTIFICATION['support_email_subject_on_failure'] += ' (TEST, PLEASE IGNORE)'
-CANVAS_EMAIL_NOTIFICATION['support_email_address'] = 'tltqaemails@g.harvard.edu'
-
 INSTALLED_APPS += ('debug_toolbar', 'sslserver')
 
 MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
--- icommons_ext_tools/urls.py
+++ icommons_ext_tools/urls.py
@@ -1,10 +1,8 @@
 from django.conf.urls import (patterns, include, url)
 from qualtrics_link import urls as ql_urls
-from canvas_course_site_wizard import urls as ccsw_urls
 
 urlpatterns = patterns(
     '',
-    url(r'^ext_tools/canvas-course-site-wizard/', include(ccsw_urls)),
     url(r'^ext_tools/not_authorized/', 'icommons_ui.views.not_authorized', name="not_authorized"),
     url(r'^ext_tools/pin/', include('icommons_common.auth.urls', namespace="pin")),
     url(r'^ext_tools/qualtrics_link/', include(ql_urls, namespace="ql")),
