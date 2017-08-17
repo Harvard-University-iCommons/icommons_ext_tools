36c5669121dd2db6cb6db61fb35b7a8a460ca873
Thornton, Christopher <christopher_thornton@harvard.edu>
2017-08-17 14:00:17 -0400

    Added stats function to show upcoming changes, added update_users function

--- qualtrics_link/management/commands/qualtrics.py
+++ qualtrics_link/management/commands/qualtrics.py
@@ -1,30 +1,31 @@
-from django.core.management.base import BaseCommand, CommandError
-import pprint
-import logging
-from qualtrics_link import util
-from qualtrics_link.models import SchoolCodeMapping
-from icommons_common.models import Person
 import json
-import pickle
+import logging
+import pprint
 
+from django.core.management.base import BaseCommand
+
+from icommons_common.models import Person
+from qualtrics_link import util
 
 logger = logging.getLogger(__name__)
-pp = pprint.PrettyPrinter(indent=4)
 
 
 class Command(BaseCommand):
-    help = 'Help text'
+    help = 'This command works in three steps; ' \
+           '1. Get all Qualtrics users; ' \
+           '2. Find those that need to be updated;' \
+           '3. Update the users from step 2 via the Qualtrics API.'
 
     def add_arguments(self, parser):
-        parser.add_argument('--dry-run', action='store_true', help='helper text placeholder')
         parser.add_argument('--get-users', action='store_true', help='Query Qualtrics to get all users and store them '
                                                                      'in a data.json file.')
+        parser.add_argument('--filter-users', action='store_true', help='Filters the data.json file to produce a new'
+                                                                        'file containing only users that need to be '
+                                                                        'updated.')
         parser.add_argument('--perform-update', action='store_true', help='Perform the update of Qualtrics user using '
                                                                           'the filtered list file.')
-        parser.add_argument('--filter-users', action='store_true', help='Filters the data.json file to produce a new'
-                                                                       'file containing only users that need to be '
-                                                                       'updated.')
-        parser.add_argument('--stats', action='store_true', help='')
+        parser.add_argument('--stats', action='store_true', help='Provide information regarding the changes that are '
+                                                                 'to be made during an update.')
 
     def handle(self, *args, **options):
         if options['get_users']:
@@ -53,13 +54,30 @@ class Command(BaseCommand):
         print 'Total active users: %d' % count
         print 'Total inactive users: %d' % (total_users - count)
 
+        filtered_file = open('filtered.json', 'r')
+        filtered_data = json.load(filtered_file)
 
-    def update_users(self):
+        for update in filtered_data:
+            print
+            print 'User %s %s with HUID: %s has the following updates:' % (update['changes']['user']['first_name'],
+                                                                           update['changes']['user']['last_name'],
+                                                                           update['changes']['user']['huid'])
+            print 'Role is changing from %s => %s' % (update['changes']['previous_data']['role'],
+                                                      update['changes']['new_data']['role'], )
+            print 'Division is changing from %s => %s' % (update['changes']['previous_data']['division'],
+                                                          update['changes']['new_data']['division'], )
+            print
+
+    @staticmethod
+    def update_users():
         """
         Will go through the list of users in the filtered file and update the users information via the Qualtrics API.
         """
-        pass
+        filtered_file = open('filtered.json', 'r')
+        filtered_data = json.load(filtered_file)
 
+        for user in filtered_data:
+            util.update_qualtrics_user(user_id=user['user_id'], division=user['division'], role=user['role'])
 
     @staticmethod
     def filter_users():
@@ -67,6 +85,12 @@ class Command(BaseCommand):
         student_user_type = 'UT_787UadC574xhxgU'
         brand_admin = 'UT_BRANDADMIN'
         user_type_list = [employee_user_type, student_user_type, brand_admin]
+
+        user_type_mapping = {
+            'employee': 'UT_egutew4nqz71QgI',
+            'student': 'UT_787UadC574xhxgU',
+            'brand administrator': 'UT_BRANDADMIN'
+        }
 
         # Mapping of a division to its Qualtrics ID
         division_mapping = {
@@ -89,6 +113,40 @@ class Command(BaseCommand):
             'GSE-PPE [no longer used]': 'DV_0vsxWeIjXJWeS21'
         }
 
+        #####
+        # This section contains reverse lookups based on Qualtrics specific ID's
+        # This is used to create a readable format when outputting to console.
+        #####
+
+        reverse_user_types = {
+            'UT_egutew4nqz71QgI': 'employee',
+            'UT_787UadC574xhxgU': 'student',
+            'UT_BRANDADMIN': 'Brand Admin'
+        }
+
+        # Reverse mapping to translate the Qualtrics division code into a readable division
+        reverse_division_mapping = {
+            'DV_0uG93Am70qIFb00': 'FAS',
+            'DV_eesMPIncvHA270U': 'GSE',
+            'DV_cvfNy3UwERh9IcA': 'HSPH',
+            'DV_1zu8x43ZIyqzWlu': 'Other',
+            'DV_bdu3uP2WTYThpOY': 'HKS',
+            'DV_cSx7CCmUZ1DaS3i': 'EXT',
+            'DV_6DN9Q7jTRzsxgHy': 'HLS',
+            'DV_77MUQ7NsyaGcQU4': 'HUIT',
+            'DV_7V89XC1uxWU2ODW': 'GSD',
+            'DV_6Fhm425s7ozZM5D': 'Central Administration',
+            'DV_5o8WAy3WJXLNX2Q': 'HDS',
+            'DV_1WSu6zRMeNx6ZYU': 'HAA (Alumni Assoc.)',
+            'DV_8dpaRpPHqefdNAx': 'VPAL Research and Affiliates',
+            'DV_1Ro0atRhq0UV9ti': 'Berkman',
+            'DV_agzgkeDIaZPEJHD': 'Radcliffe',
+            'DV_23NVy6XjBHhOXxX': 'API Div',
+            'DV_0vsxWeIjXJWeS21': 'GSE-PPE [no longer used]',
+            'None': 'None'
+        }
+        #####
+
         data_file = open('data.json', 'r')
         data = json.load(data_file)
         print('%d records in file' % len(data))
@@ -97,9 +155,11 @@ class Command(BaseCommand):
         update_list = []
 
         # How many users to process at each run time.
-        slice_amount = 15
+        slice_amount = 25
 
+        position = 1
         for q_person in data[:slice_amount]:
+            print 'Processing user %d out of %d' % (position, slice_amount)
             q_id = q_person['id']
             q_username = q_person['username']
             q_division = q_person['divisionId']
@@ -125,7 +185,7 @@ class Command(BaseCommand):
             # Compare the Qualtrics data against the LDAP People data
             if matching_person is not None:
                 person_details = util.get_person_details(matching_person.univ_id)
-                p_role = person_details.role
+                p_role = user_type_mapping[person_details.role]
                 p_division = division_mapping[person_details.division]
 
                 update_person = False
@@ -138,27 +198,51 @@ class Command(BaseCommand):
                     update_dict['division'] = p_division
                     update_person = True
 
-                # If the Person is not a Student or Employee, then they need to be updated
+                # If the Person is not a Student or Employee, or Brand Admin then they need to be updated
                 if q_role not in user_type_list:
                     update_dict['user_type'] = p_role
                     update_person = True
 
                 # If the update person bool has been set to True, then get the values from the update dict to be
-                # added to the update list. Provide the person
+                # added to the update list.
                 if update_person:
-                    # TODO make check for brand admin
+                    # If the only update is the users division and they are a brand admin, the role will default to the
+                    # persons role of Employee. If they are a brand admin in Qualtrics, set their role to stay the same
+                    # in the default case.
+                    if q_role == brand_admin:
+                        p_role = brand_admin
 
+                    # Use the Person record value as a default in the case of only one field needing to be updated.
                     update_entry = {
                         'user_id': update_dict.get('user_id'),
                         'division': update_dict.get('division', p_division),
-                        'role': update_dict.get('role', p_role)
+                        'role': update_dict.get('role', p_role),
                     }
 
-                    print
-                    print 'User requires an update'
-                    print update_entry
-                    print
+                    # Have a readable/translated changes portion or the entry that can be output to console to show what
+                    # has changed
+                    if q_division is None:
+                        q_division = 'None'
+                    update_entry['changes'] = {
+                        'user': {
+                            'huid': person_details.id,
+                            'first_name': person_details.first_name,
+                            'last_name': person_details.last_name
+                        },
+                        'previous_data': {
+                            'division': reverse_division_mapping[q_division],
+                            'role': reverse_user_types[p_role]
+                        },
+                        'new_data': {
+                            'division': reverse_division_mapping[update_entry['division']],
+                            'role': reverse_user_types[update_entry['role']]
+                        }
+                    }
+
+                    print 'Found user requiring update'
                     update_list.append(update_entry)
+
+            position += 1
 
         print
         print 'Update statistics'
@@ -202,4 +286,3 @@ class Command(BaseCommand):
         # Create JSON file with the user info
         with open('data.json', 'w+') as outfile:
             json.dump(all_users_list, outfile)
-
