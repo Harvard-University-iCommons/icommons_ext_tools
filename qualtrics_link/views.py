277e074c13fcffa82f2c825302e12af67abafbed
Chris Thornton <christopher_thornton@harvard.edu>
2017-10-13 12:52:30 -0400

    Added division mapping to launch view

--- qualtrics_link/views.py
+++ qualtrics_link/views.py
@@ -97,7 +97,7 @@ def launch(request):
                                                  person_details.last_name,
                                                  person_details.email,
                                                  person_details.role,
-                                                 person_details.division)
+                                                 util.DIVISION_MAPPING[person_details.division])
         qualtrics_link = util.get_qualtrics_url(key_value_pairs)
         logger.info("{}\t{}\t{}\t{}".format(current_date, client_ip, person_details.role, person_details.division))
 
