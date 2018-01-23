020a83739b0928beed792c2122417e0590d4f231
Chris-Thornton-Harvard <christopher_thornton@harvard.edu>
2018-01-23 12:46:28 -0500

    Qualtrics admin form

--- qualtrics_link/forms.py
+++ qualtrics_link/forms.py
@@ -1,5 +1,13 @@
 from django import forms
+from qualtrics_link import util
 
 
 class SpoofForm(forms.Form):
     huid = forms.CharField(max_length=15, required=False)
+
+
+class QualtricsUserAdminForm(forms.Form):
+    division = forms.ChoiceField(choices=util.DIVISION_CHOICES)
+    role = forms.ChoiceField(choices=util.ROLE_CHOICES)
+    manually_updated = forms.ChoiceField(choices=(('True', 'True'), ('False', 'False')))
+
